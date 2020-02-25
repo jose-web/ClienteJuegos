949343a89526fee3f9751bdd2f00486f094e2080
RCaracuel <rosa.caracuel@gmail.com>
2020-02-25 19:40:04 +0100

    Subimos la clase conexión con los parámetros correspondientes para la correcta conexión a la bbdd

--- src/main/java/daw/clientejuegosModelo/Conexion.java
+++ src/main/java/daw/clientejuegosModelo/Conexion.java
@@ -5,10 +5,46 @@
  */
 package daw.clientejuegosModelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Rosa
  */
 public class Conexion {
-    
+     private static final String SERVIDOR = "jdbc:mysql://192.168.1.127/";
+    private static final String NOMBRE_BASE_DATOS = "clientejuegos";
+    private static final String USER = "jose";
+    private static final String PASS = "josefa";
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    private static Connection instancia = null;
+
+    private Conexion() {
+
+    }
+
+    // Método de clase para acceder a la instancia del objeto Connection
+    public static Connection getInstance() {
+        // Si el objeto Connection no está creado, se crea
+        if (instancia == null) {
+            try {
+                Class.forName(DRIVER);
+                // Se crea el objeto Connection	
+                instancia = DriverManager.getConnection(SERVIDOR + NOMBRE_BASE_DATOS, USER, PASS);
+
+                System.out.println("Conexión realizada con éxito.");
+
+            } catch (SQLException e) {
+                // Error en la conexión
+                System.out.println("Conexión fallida: " + e.getMessage());
+            } catch (ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        return instancia;
+    }
+
 }

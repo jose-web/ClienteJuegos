b6ef35119d339b4a5bef0aa98b3ab7cf370cf7ca
RCaracuel <rosa.caracuel@gmail.com>
2020-02-28 20:58:26 +0100

    Añadimos servlet para cuando pulsemos sobre la imagen/botón de un juego nos dirija hacia su página de información (juego.jsp), para ello le pasamos una variable de sesión y posteriormente haremos la llamada a la bbdd de dicho juego.

---
+++ src/main/java/daw/clientejuegos/controlador/Juego.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.clientejuegos.controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Rosa
+ */
+public class Juego extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            ServletContext contexto = request.getServletContext();
+
+            // Obtengo la sesion de la petición HTTP, si existe. 
+            // Con true, si no está creada se crea
+            HttpSession sesion = request.getSession(true);
+            String idJuego = request.getParameter("juego");
+            // Asigno ese nombre del visitante al atributo de la sesión y así lo puedo usar en la vista
+            sesion.setAttribute("idJuego", idJuego);
+            RequestDispatcher despachador = contexto.getRequestDispatcher("/juego.jsp");
+            // El método fordward permite hacer la redirección a otro servlet o a una
+            // vista y se pasa tanto el objeto petición como el objeto respuesta
+            // para que el nuevo recurso pueda hacer uso de ellos
+            despachador.forward(request, response);
+
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/juego.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : juego
+    Created on : 28 feb. 2020, 20:08:48
+    Author     : Rosa
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>id ${sessionScope['idJuego']}<h1>
+    </body>
+</html>
